bfb8fdd3493b04266085916202c5703b6fde9588
Daniel Vogel <dvogel@uwaterloo.ca>
2024-01-15 21:28:29 -0500

    update template

--- template/sketch.js
+++ template/sketch.js
@@ -5,47 +5,38 @@ let p = {
   numericMin: 0,
   numericMax: 100,
   numericStep: 1,
-}
+};
 
-function preload() {
-}
+function preload() {}
 
 function setup() {
-  createCanvas(400, 400)
-  
+  createCanvas(400, 400);
+
   // add params to a GUI
   createParamGui(p, paramChanged);
   _paramGui.setPosition(10, 10); // can customize where GUI is drawn
   // _paramGui.hide(); // uncomment to hide for presentations
 
   // set colour for your frame
-  select('body').style('background: #000000;')
+  select("body").style("background: #000000;");
 
   // don't centre
   // select('body').style('place-items: start start;')
-  // _paramGui.setPosition(width + 10, 10); // can customize where GUI is drawn  
-
- }
+  // _paramGui.setPosition(width + 10, 10); // can customize where GUI is drawn
+}
 
 function draw() {
-  background(240)
- 
+  background(240);
 }
 
 function keyPressed() {
-  if (key == ' ') {
+  if (key == " ") {
   }
 }
 
-function mousePressed() {
-}
+function mousePressed() {}
 
-function windowResized() {
-}
+function windowResized() {}
 
 // global callback from the settings GUI
-function paramChanged(name) {
-}
-
-
-
+function paramChanged(name) {}
--- template/utilities.js
+++ template/utilities.js
@@ -2,7 +2,6 @@
  * Various Utilities
  */
 
-
 /**
  * Simple parameter adjustment GUI
  * uses p5.gui
@@ -13,56 +12,49 @@
  */
 let _paramGui;
 function createParamGui(params, callback) {
-
   // settings gui
-  _paramGui = createGui('Settings');
+  _paramGui = createGui("Settings");
   // settingsGui.prototype.addButton("Save", function () { storeItem("params", settings) });
-  _paramGui.addObject(params)
-  if (callback)
-    _paramGui.prototype.setGlobalChangeHandler(callback)
+  _paramGui.addObject(params);
+  if (callback) _paramGui.prototype.setGlobalChangeHandler(callback);
   // settingsGui.prototype.addRange('size', 1, 64, 32, 1, function(v) { print("size changed", v) } )
-
 
   _paramGui.setPosition(width + 10, 10);
   // the 'S' key hides or shows the GUI
   _paramGui.prototype.setKey("s");
-
 }
 
-
-fps = 0
+fps = 0;
 
 function drawFps() {
-  let a = 0.01
-  fps = a * frameRate() + (1 - a) * fps
-  stroke(255)
-  strokeWeight(0.5)
-  fill(0)
-  textAlign(LEFT, TOP)
-  textSize(20.0)
-  text(this.fps.toFixed(1), 10, 10)
+  let a = 0.01;
+  fps = a * frameRate() + (1 - a) * fps;
+  stroke(255);
+  strokeWeight(0.5);
+  fill(0);
+  textAlign(LEFT, TOP);
+  textSize(20.0);
+  text(this.fps.toFixed(1), 10, 10);
 }
 
 /*
  * Simple debugging to HTML textarea
  */
-let _debugTextArea
+let _debugTextArea;
 
 function debug(s) {
-  if (typeof _debugTextArea !== 'undefined')
-    _debugTextArea.html(s + '\n', true); 
+  if (typeof _debugTextArea !== "undefined")
+    _debugTextArea.html(s + "\n", true);
 }
 
 function debugClear() {
-  if (typeof _debugTextArea !== 'undefined')  
-    _debugTextArea.html('')
+  if (typeof _debugTextArea !== "undefined") _debugTextArea.html("");
 }
 
 function createDebugWindow() {
-  _debugTextArea = createElement('textarea')
-  _debugTextArea.style('min-width: 700px; height: 400px; padding: 20px; margin: 20px;')
-  debug('** debug window created **')
+  _debugTextArea = createElement("textarea");
+  _debugTextArea.style(
+    "min-width: 700px; height: 400px; padding: 20px; margin: 20px;"
+  );
+  debug("** debug window created **");
 }
-
-
-
